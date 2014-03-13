8f632965c4ffc9c276f1cdac4055c6a9615f8cc4
Roger Wang <shypwang@gmail.com>
2014-03-13 13:27:16 +0800

    add inlucde and extend method to Class

--- compdevjs/src/core.js
+++ compdevjs/src/core.js
@@ -2,7 +2,30 @@ var Class = function() {
 	var klass = function() {
 		this.init.apply(this, arguments);
 	};
-	klass.prototype.init = function() {
+
+	// short cut for prototype
+	klass.fn = klass.prototype;
+
+	klass.fn.init = function() {
 	};
+
+	klass.extend = function(object) {
+		var i;
+		for (i in object) {
+			if (object.hasOwnProperty(i)) {
+				klass[i] = object[i];
+			}
+		}
+	};
+
+	klass.include = function(object) {
+		var i;
+		for (i in object) {
+			if (object.hasOwnProperty(i)) {
+				klass.fn[i] = object[i];
+			}
+		}
+	};
+
 	return klass;
 };
